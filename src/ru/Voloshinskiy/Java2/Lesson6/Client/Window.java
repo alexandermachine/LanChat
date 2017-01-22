674da5357a3b1373366e1e73264670768c711c09
alexandermachine <alexanderuv@mail.ru>
2017-01-22 13:45:04 +0600

    1. объеденены классы Window и ConnectToServer -> Window 2. разбил конструкор на методы, для читабельности 3. добавлена аутентификация в Window, на сервере пока нет

--- src/ru/Voloshinskiy/Java2/Lesson6/Client/Window.java
+++ src/ru/Voloshinskiy/Java2/Lesson6/Client/Window.java
@@ -6,41 +6,53 @@ import java.awt.event.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
 /**
  * Created by Yuriy Fedorovich on 07.01.2017.
  */
 public class Window extends JFrame {
+    private JTextArea corField;
+    private JPanel jpAuth;
+    private String serverHost;
+    private int serverPort;
     private Scanner dataInputStream = null;
     private PrintWriter dataOutputStream = null;
+    private Socket socket = null;
+    private String name = "somebody";
 
+    public Window(String serverHost, int serverPort) throws IOException {
+        this.serverHost = serverHost;
+        this.serverPort = serverPort;
+        initConnection();
+        initGUI();
+    }
 
-    public Window(String SERVER_HOST, int SERVER_PORT) throws IOException {
-        ConnectToServer connection = new ConnectToServer(SERVER_HOST, SERVER_PORT);
-        dataInputStream = connection.getDataInputStream();
-        dataOutputStream = connection.getDataOutputStream();
+    private void initConnection() throws IOException {
+            socket = new Socket(serverHost,serverPort);
+            dataInputStream = new Scanner(socket.getInputStream());
+            dataOutputStream = new PrintWriter(socket.getOutputStream());
+    }
 
+    private void initGUI(){
         setTitle("My chat");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(500, 200, 500, 400);
         setLayout(new BorderLayout());
+        initTopPanel();
+        initCorrespondenceField();
+        initMessageField();
+        setVisible(true);
+    }
 
-
-        JPanel jpCor = new JPanel();
-        add(jpCor,BorderLayout.CENTER);
-        JPanel jpMes = new JPanel();
-        add(jpMes,BorderLayout.SOUTH);
-
+    private void initTopPanel(){
         JMenuBar mainMenu = new JMenuBar();
         JMenu mFile = new JMenu("File");
         JMenuItem miFileSetName = new JMenuItem("Set name");
         JMenuItem miFileExit = new JMenuItem("Exit");
-
         setJMenuBar(mainMenu);
-
         mainMenu.add(mFile);
-
         mFile.add(miFileSetName);
         mFile.addSeparator(); // разделительная линия
         mFile.add(miFileExit);
@@ -48,7 +60,7 @@ public class Window extends JFrame {
         miFileSetName.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                connection.setName((String) JOptionPane.showInputDialog(null));
+                setName((String) JOptionPane.showInputDialog(null));
             }
         });
 
@@ -58,29 +70,26 @@ public class Window extends JFrame {
                 System.exit(0);
             }
         });
+    }
 
+    private void initCorrespondenceField(){
+        JPanel jpCor = new JPanel();
+        add(jpCor,BorderLayout.CENTER);
 
-
-        //***********переписка***********
         jpCor.setLayout(new BorderLayout());
-        JTextArea jta = new JTextArea();
-        jta.setLineWrap(true);
-        jta.setWrapStyleWord(true);
-        jta.setEditable(false);
-        JScrollPane jsp = new JScrollPane(jta);
+        corField = new JTextArea();
+        corField.setLineWrap(true);
+        corField.setWrapStyleWord(true);
+        corField.setEditable(false);
+        JScrollPane jsp = new JScrollPane(corField);
         jpCor.add(jsp);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true)
-                    if (dataInputStream.hasNext())
-                        jta.append(dataInputStream.nextLine()+'\n');
-            }
-        }).start();
-        //*******************************
 
-        //*******Ввод сообщения**********
+    }
+
+    private void initMessageField(){
+        JPanel jpMes = new JPanel();
+        add(jpMes,BorderLayout.SOUTH);
         jpMes.setLayout(new BorderLayout());
         JTextField jtf = new JTextField("Введите текст");
         jtf.addFocusListener(new FocusListener() {
@@ -96,30 +105,86 @@ public class Window extends JFrame {
         jtf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                connection.sendMsg(jtf.getText());
-                //dataOutputStream.println(jtf.getText()+'\n');
+                sendMsg(jtf.getText());
                 jtf.setText("");
             }
         });
-
         JButton jbSend = new JButton("Отправить");
         jbSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                connection.sendMsg(jtf.getText());
-                //dataOutputStream.println(jtf.getText()+'\n');
+                sendMsg(jtf.getText());
                 jtf.setText("");
             }
         });
         jpMes.add(jtf,BorderLayout.CENTER);
         jpMes.add(jbSend,BorderLayout.EAST);
-        //*******************************
-
-
-        setVisible(true);
     }
 
+    private void authentication(){
+        jpAuth = new JPanel();
+        jpAuth = new JPanel(new GridLayout(2,3));
+
+        JTextField loginField = new JTextField();
+        loginField.setToolTipText("Enter your Login here");
+        jpAuth.add(new JLabel("Login: "));
+        jpAuth.add(loginField);
+
+        JButton signInButton = new JButton("Sing In");
+        jpAuth.add(signInButton);
+
+        JTextField passwordField = new JTextField();
+        passwordField.setToolTipText("Enter your Password here");
+        jpAuth.add(new JLabel("Password: "));
+        jpAuth.add(passwordField);
+
+        signInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendAuthCommand(loginField.getText(), passwordField.getText());
+            }
+        });
+        add(jpAuth,BorderLayout.CENTER);
+    }
+
+    private void sendAuthCommand(String login, String password){
+        dataOutputStream.println("___Login:___" + login + "___Password___" + password);
+        dataOutputStream.flush();
+    }
+
+    private void serverListener(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msg;
+                while(true) {
+                    if (dataInputStream.hasNext()) {
+                        msg = dataInputStream.nextLine();
+
+                        if (msg.equalsIgnoreCase("SignIn_OK")) {
+                            JOptionPane.showMessageDialog(null, "Аутентификация прошла успешно.");
+                            jpAuth.setVisible(false);
+                        }
+                        else if (msg.equalsIgnoreCase("SignIn_Fail")){
+                            JOptionPane.showMessageDialog(null,"Неверная пара логин/пароль, попробуйте ещё");
+                        }
+                        else
+                            corField.append(dataInputStream.nextLine() + '\n');
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void sendMsg(String msg){
+        dataOutputStream.println(name+": "+msg);
+        dataOutputStream.flush();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     private void writeToFile(String str){
         PrintWriter pw = null;
